f93fa4e86017bc105b32dc425138406c27d0e524
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2022-12-02 18:50:08 +0200

    Update Assistant.h

--- Assistant.h
+++ Assistant.h
@@ -12,19 +12,17 @@
 #ifndef Assistant_H
 #define Assistant_H
 
-#include "Settings.h"
-
-#define NUM_VOICES 5 
-#define DEFAULT_VOICE "f4" 
-#define YES "y"
-#define NO "n"
-
 /*---------- class of the virtual assistant ----------*/
 class Assistant
 {
 private:
-    const string FEMALE_VOICES[NUM_VOICES] {"f1", "f2", "f3", "f4", "f5"}; //Female voices array
-    const string MALE_VOICES[NUM_VOICES] {"m1", "m2", "m3", "m4", "m5"}; //Male voices array
+    const int NUMBER_OF_VOICES = 5;
+    const string DEFAULT_VOICE = "f4";
+    const string YES = "y";
+    const string NO = "n";
+    const string FEMALE_VOICES[NUMBER_OF_VOICES] {"f1", "f2", "f3", "f4", "f5"}; //Female voices array
+    const string MALE_VOICES[NUMBER_OF_VOICES] {"m1", "m2", "m3", "m4", "m5"}; //Male voices array
+    
     string voice;
 
     string insert_valid_choice(const string &, const string &);
